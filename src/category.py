2d4b5ad820f7eead47a86a2b661eeff44b0ea177
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-07-25 23:12:13 +0300

    Добавлен функционал в класс Category

--- src/category.py
+++ src/category.py
@@ -1,10 +1,13 @@
+import os
+
+from src.product import Product
 from src.readers import read_json_file
 
 
 class Category:
     name: str
     description: str
-    products: list
+    __products: list
 
     category_count: int = 0
     product_count: int = 0
@@ -12,18 +15,26 @@ class Category:
     def __init__(self, name, description, product):
         self.name = name
         self.description = description
-        self.products = product
+        self.__products = product
 
         Category.category_count += 1
         Category.product_count += len(product)
 
+    def add_product(self, product: Product) -> None:
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def get_list_products(self):
+        return [f"{elem['name']}, {elem['price']} руб. Остаток: {elem['quantity']} шт" for elem in self.__products]
+
 
 def create_category_from_json_file(path_to_file: str = "") -> list:
     """Создает объекты класса на основании информации их json-файла"""
-    data_ = read_json_file(path_to_file)
+    data_ = read_json_file()
     category_list = []
 
     for elem in data_:
-        category_list.append(Category(elem["name"], elem["description"], elem["product"]))
+        category_list.append(Category(elem["name"], elem["description"], elem["products"]))
 
     return category_list
