9952ffa146825183f6dc79bb3c4c1b829a70ce29
Ananim4eg <ananim4eg93@gmail.com>
2025-08-06 20:04:48 +0300

    edit README.md

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ if __name__ == '__main__':
 
     print(product1.name)
     print(product1.description)
-    print(product1.price)
+    print(product1.product_price)
     print(product1.quantity)
 
     print(product2.name)
     print(product2.description)
-    print(product2.price)
+    print(product2.product_price)
     print(product2.quantity)
 
     print(product3.name)
     print(product3.description)
-    print(product3.price)
+    print(product3.product_price)
     print(product3.quantity)
 
     category1 = Category("Смартфоны",
@@ -29,7 +29,7 @@ if __name__ == '__main__':
 
     print(category1.name == "Смартфоны")
     print(category1.description)
-    print(len(category1.products))
+    print(len(category1.get_list_products))
     print(category1.category_count)
     print(category1.product_count)
 
@@ -40,8 +40,8 @@ if __name__ == '__main__':
 
     print(category2.name)
     print(category2.description)
-    print(len(category2.products))
-    print(category2.products)
+    print(len(category2.get_list_products))
+    print(category2.get_list_products)
 
     print(Category.category_count)
     print(Category.product_count)
